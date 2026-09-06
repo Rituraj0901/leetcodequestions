8da18f518fb39fd48d06b5a7e5276303cf594dac
Rituraj0901 <riturajrajawatsingh6265@gmail.com>
2026-09-06 12:43:16 +0530

    Time: 296 ms (5.12%), Space: 166.5 MB (57.6%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -3,7 +3,7 @@ class Solution {
         List<List<Integer>> list = new ArrayList<>();
         List<Integer> first = new ArrayList<>();
         List<Integer> second = new ArrayList<>(); 
-        HashMap <Integer,Integer> mpp = new HashMap<>();
+        TreeMap <Integer,Integer> mpp = new TreeMap<>();
 
         for (int [] match: matches){
             int win = match[0];
@@ -20,8 +20,8 @@ class Solution {
             if(mpp.get(it) == 0) first.add(it);
             else if (mpp.get(it) == 1) second.add(it);
         }
-        Collections.sort(first);
-        Collections.sort(second);
+        //Collections.sort(first); agr ye use nhi krna hai to upr hash map ki jagah tree map use krlo for sorting
+        //Collections.sort(second);
         list.add(first);
         list.add(second);
 
